1dba3137d8219754981579f1d44094e477034504
Petrosceac Bogdan Ciprian <b.petrosceac@gmail.com>
2021-12-04 17:11:07 +0200

    Initialize Project.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TestPage from "./Components/testPage";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <TestPage />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
